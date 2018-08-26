314ed0e03174667d69a9aef875ace3ef7501da2d
Xander Luciano <xanderluciano@gmail.com>
2018-08-26 02:41:50 -0700

    Add autorun to vue-cli script

--- meta.js
+++ meta.js
@@ -1,3 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+const {
+    sortDependencies,
+    installDependencies,
+    runLintFix,
+    printMessage,
+} = require('./utils');
+
+const pkg = require('./package.json');
+
 module.exports = {
   helpers: {
     raw: function(options) {
@@ -14,7 +26,7 @@ module.exports = {
       'type': 'string',
       'required': false,
       'message': 'Project description',
-      'default': 'Nuxt.js + Vuetify.js project'
+      'default': 'NCViewer Vuetify + Nuxt Template'
     },
     author: {
       'type': 'string',
@@ -23,8 +35,50 @@ module.exports = {
     alacarte: {
       'type': 'confirm',
       'message': 'Use a-la-carte components?',
-      'default': false
+      'default': true
+    },
+    autoInstall: {
+      when: 'isNotTest',
+      type: 'list',
+      message: 'Should we run `npm install` for you after the project has been created? (recommended)',
+      choices: [
+        {
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm',
+        },
+        {
+          name: 'Yes, use Yarn (not recommended)',
+          value: 'yarn',
+          short: 'yarn',
+        },
+        {
+          name: 'No, I will handle that myself',
+          value: false,
+          short: 'no',
+        },
+      ],
+    },
+  },
+  // completeMessage: '{{#inPlace}}To get started:\n\n  npm install\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev{{/inPlace}}',
+  complete: function(data, { chalk }) {
+    const green = chalk.green;
+    sortDependencies(data, green);
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green)
+        .then(() => {
+          return runLintFix(cwd, data, green);
+        })
+        .then(() => {
+          printMessage(data, green);
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e);
+        })
+    } else {
+      printMessage(data, chalk);
     }
   },
-  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
